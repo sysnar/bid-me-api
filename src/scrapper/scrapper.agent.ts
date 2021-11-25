b9c3f89ed1ad13937edc1f51b790ecddbf681edb
sysnar <sysn4r@gmail.com>
2021-11-25 21:47:50 +0900

    Add agent scrapping bid info logic

--- src/scrapper/scrapper.agent.ts
+++ src/scrapper/scrapper.agent.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { FsExplorer } from './fsExplorer.model';
 import axios from 'axios';
+import { Iconv } from 'iconv';
+import cheerio from 'cheerio';
 
 @Injectable()
 export class Agent {
   private _queue;
-  constructor(private fsExplorer: FsExplorer) {}
+  private iconv;
+  constructor(private fsExplorer: FsExplorer) {
+    this.iconv = new Iconv('EUC-KR', 'utf-8//translit//ignore');
+  }
 
   // Scrap bid data from queue
   readQueue(path: string, file: string): string[] {
@@ -13,12 +18,28 @@ export class Agent {
     return this.fsExplorer.fileToArray(path, file);
   }
 
-  scrapData = async (queue: string[]) => {
-    for (let link of queue) {
-      await axios.get(link);
-
-      // Store bid data to crawlled.txt & DB
+  async getHTML(url) {
+    try {
+      const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+      return this.iconv.convert(data).toString();
+    } catch (error) {
+      console.log(error);
     }
-    // update data in queue.txt
-  };
+  }
+
+  parseText(plainText: String) {
+    const a = plainText.split('\t');
+    return;
+  }
+
+  async scrapData(queue: string[]) {
+    for (let url of queue) {
+      // Store bid data to crawlled.txt & DB
+      const pageData = await this.getHTML(url);
+      const $ = cheerio.load(pageData);
+      const title = $('#container > div:nth-child(8) > table > tbody > tr:nth-child(3) > td > div');
+      console.log(title.text().trim().split(' '));
+    }
+    // update data in crawled.txt
+  }
 }
