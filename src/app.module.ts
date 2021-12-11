3f3c3e1bc1513ba71ce7d45491208ccdbb870e29
sysnar <sysn4r@gmail.com>
2021-12-12 07:27:44 +0900

    ADD initial user api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { ScrapperModule } from './scrapper/scrapper.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { getConfigModule, getTypeORMModule } from './config';
 import { BidDataModule } from './api/bid.data/bid.data.module';
+import { UserModule } from './api/user/user.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), ScrapperModule, getConfigModule(), getTypeORMModule(), BidDataModule],
+  imports: [ScheduleModule.forRoot(), ScrapperModule, getConfigModule(), getTypeORMModule(), BidDataModule, UserModule],
 })
 export class AppModule {}
