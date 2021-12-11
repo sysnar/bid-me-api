ee8d58f584721437b8e553b9d7fd18f0f2ce90b5
sysnar <sysn4r@gmail.com>
2021-12-12 07:34:42 +0900

    DELETE unusing scrapper module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ScrapperModule } from './scrapper/scrapper.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { getConfigModule, getTypeORMModule } from './config';
 import { BidDataModule } from './api/bid.data/bid.data.module';
 import { UserModule } from './api/user/user.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), ScrapperModule, getConfigModule(), getTypeORMModule(), BidDataModule, UserModule],
+  imports: [ScheduleModule.forRoot(), getConfigModule(), getTypeORMModule(), BidDataModule, UserModule],
 })
 export class AppModule {}
--- src/scrapper/dto/scrapper-config.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class ScrapperConfigDto {
-  @IsNotEmpty()
-  @IsString()
-  initURL: string;
-}
