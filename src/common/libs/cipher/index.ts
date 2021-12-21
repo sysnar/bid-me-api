97982b017ece14ac774482ecda5ffdd200cc0f23
sysnar <sysn4r@gmail.com>
2021-12-21 15:51:03 +0900

    FIX ciper lib comments

--- src/common/libs/cipher/index.ts
+++ src/common/libs/cipher/index.ts
@@ -1,12 +1,14 @@
 import * as bcrypt from 'bcrypt';
 
 export class Cipher {
+  // 파라미터로 전달받은 평문을 암호화하여 반환
   static async ENCRYPT(plainText: string, saltRound = 10): Promise<string> {
-    const salt = await bcrypt.genSalt();
+    const salt = await bcrypt.genSalt(saltRound);
     const hashedPassword = await bcrypt.hash(plainText, salt);
     return hashedPassword;
   }
 
+  // 기존 암호화된 값와 평문을 비교하여 결과값을 반환 (true / false)
   static async COMPARE(plainText: string, encrytText: string): Promise<boolean> {
     return await bcrypt.compare(plainText, encrytText);
   }
